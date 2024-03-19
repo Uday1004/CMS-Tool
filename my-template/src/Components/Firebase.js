b535ce195f7f75da1154602ce8758809e25f19b6
uday1004 <udaysolanki530@gmail.com>
2024-03-19 16:02:51 +0530

    complete: collection and rendering

---
+++ my-template/src/Components/Firebase.js
@@ -0,0 +1,18 @@
+ 
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyAf1O757SXCqqq0ZdcJ3tfAJnj7B531XLA",
+  authDomain: "content-management-tool-3b2a0.firebaseapp.com",
+  databaseURL: "https://content-management-tool-3b2a0-default-rtdb.firebaseio.com",
+  projectId: "content-management-tool-3b2a0",
+  storageBucket: "content-management-tool-3b2a0.appspot.com",
+  messagingSenderId: "260099902710",
+  appId: "1:260099902710:web:dc369a3cedc26f1161b0d7",
+  measurementId: "G-PJPS0TPTCK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app)
